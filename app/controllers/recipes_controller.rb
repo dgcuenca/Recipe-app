e12339db1b85877141c79bfcdcd0384ee7912220
diego <dgcuenca@gmail.com>
2023-04-18 15:47:18 -0500

    Create a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,10 +12,24 @@ class RecipesController < ApplicationController
   end
 
   def create
-    # create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: 'Recipe was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
     # destroy
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :name, :preparation_time, :cooking_time, :description)
+  end
+
 end
