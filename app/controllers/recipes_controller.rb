a9c6730017bec5845b89f8e016403ad97b73f639
belaymit <2belamit@gmail.com>
2023-04-18 16:49:59 +0300

    Generated controllers

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,21 @@
+class RecipesController < ApplicationController
+  def index
+    # index
+  end
+
+  def show
+    # show
+  end
+
+  def new
+    # new
+  end
+
+  def create
+    # create
+  end
+
+  def destroy
+    # destroy
+  end
+end
---
+++ app/helpers/pulic_recipes_helper.rb
@@ -0,0 +1,2 @@
+module PulicRecipesHelper
+end
